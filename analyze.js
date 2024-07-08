1884c9b5510ae327d83b50809fe4b88645c750d7
Tim Greller <timlg07@gmail.com>
2024-07-08 17:30:36 +0200

    make analyze & prettify commands

--- analyze.js
+++ analyze.js
@@ -1,4 +1,7 @@
+require('process').chdir('./analyzer');
+
 require('./analyzer/timeUntilActivation')
 require('./analyzer/levelsReached')
 require('./analyzer/timeUntilPass')
 require('./analyzer/attemptsUntilPass')
+require('./analyzer/attemptsUntilActivation')
