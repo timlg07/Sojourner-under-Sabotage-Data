ba398cba940ee22f8215968c45b936e9cce013e8
Tim Greller <timlg07@gmail.com>
2024-07-09 16:39:55 +0200

    extract test src code at activation

---
+++ analyzer/codeAtActivation.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+/** @type {Array<{eventType: string, timestamp: number, user: string, data: any}>} */
+const data = require('../data.pretty.json');
+/** @type {string[]} */
+const usernames = require('../usernames.json');
+
+
+// Time until the tests are activated per user and component
+const finalCode = Object.entries(data
+    .filter(item => item.eventType === 'ComponentTestsActivatedEvent' || item.eventType === 'test-modified')
+    .reduce((acc, item) => {
+        const c = item.data.componentName;
+        if (!acc[item.user][c]) {
+            acc[item.user][c] = {
+                endTime: Number.POSITIVE_INFINITY,
+                modifications: []
+            }
+        }
+        if (item.eventType === 'ComponentTestsActivatedEvent') {
+            acc[item.user][c].endTime = item.timestamp;
+        } else if (acc[item.user][c].endTime > item.timestamp) {
+            acc[item.user][c].modifications.push(item);
+        }
+        return acc;
+    }, usernames.map(name => ({ [name]: {} })).reduce((acc, item) => ({ ...acc, ...item }), {}))
+).reduce((acc, item) => {
+    const name = item[0];
+    const components = item[1];
+    acc[name] = Object.entries(components).reduce((acc, item) => {
+        const c = item[0];
+        const data = item[1];
+        acc[c] = {sourceCode: data.modifications.reduce((acc, item) => {
+            if (item.timestamp >= acc.timestamp) {
+                return item;
+            }
+            return acc;
+        }, {timestamp: 0}).data.sourceCode};
+        acc[c].wasActivated = data.endTime !== Number.POSITIVE_INFINITY;
+        return acc;
+    }, {});
+    return acc;
+}, {});
+fs.writeFileSync('../finalCode.json', JSON.stringify(finalCode, null, 4), 'utf8');
+
+const codeAtActivation = Object.entries(finalCode).reduce((acc, item) => {
+    const name = item[0];
+    const components = item[1];
+    acc[name] = Object.entries(components).reduce((acc, item) => {
+        const c = item[0];
+        const data = item[1];
+        if (data.wasActivated) {
+            acc[c] = data.sourceCode;
+        }
+        return acc;
+    }, {});
+    return acc;
+}, {});
+fs.writeFileSync('../codeAtActivation.json', JSON.stringify(codeAtActivation, null, 4), 'utf8');
