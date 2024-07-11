70724d8ec837c27e2b2a55c49e58907b0ad7654a
Tim Greller <timlg07@gmail.com>
2024-07-11 20:58:31 +0200

    extract the print statements per component additional to per modification & extract all print contents

--- analyzer/printStatementsAdded.js
+++ analyzer/printStatementsAdded.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 const DiffMatchPatch = require('diff-match-patch');
 const attemptsUntilFixed = require('../attemptsUntilFixed_detailed.json');
 
+let allPrints = [];
+
 const filterPrints = str => str.match(/System\.out\.print(ln)?\((.*?)\);/g) ?? [];
 const extractPrintContents = str => str.match(/System\.out\.print(?:ln)?\((.*?)\);/)[1];
+const merge = (a, b, predicate = (a, b) => a === b) => {
+    const c = [...a];
+    b.forEach((bItem) => (c.some((cItem) => predicate(bItem, cItem)) ? null : c.push(bItem)))
+    return c;
+}
 
 /**
  * @param {string} patch Diff patch string
@@ -27,6 +34,8 @@ const amountOfPrints = (patch) => {
 
             acc.prints += insertions.length;
             acc.contents.push(...contents);
+
+            allPrints = merge(allPrints, contents);
         }
         return acc;
     }, {
@@ -35,6 +44,28 @@ const amountOfPrints = (patch) => {
     });
 }
 
+const printsAddedPerComponent = Object.entries(attemptsUntilFixed).reduce((acc, [user, item]) => {
+    acc[user] = Object.entries(item).reduce((acc, [component, item]) => {
+        if (item === 'not fixed') return acc;
+
+        acc[component] = item.modifications.reduce((acc, {patch}) => {
+            const {prints, contents} = amountOfPrints(patch);
+            acc.maxPrints = Math.max(acc.maxPrints, prints);
+            acc.contents = merge(acc.contents, contents);
+            return acc;
+        }, {
+            maxPrints: 0,
+            contents: []
+        });
+        return acc;
+    }, {});
+    return acc;
+}, {});
+
+fs.writeFileSync('../printsAddedPerComponent.json', JSON.stringify(printsAddedPerComponent, null, 4), 'utf8');
+
+
+// todo: remove?
 const printsAddedPerRun = Object.entries(attemptsUntilFixed).reduce((acc, [user, item]) => {
     acc[user] = Object.entries(item).reduce((acc, [component, item]) => {
         if (item === 'not fixed') return acc;
@@ -49,3 +80,7 @@ const printsAddedPerRun = Object.entries(attemptsUntilFixed).reduce((acc, [user,
 }, {});
 
 fs.writeFileSync('../printsAddedPerRun.json', JSON.stringify(printsAddedPerRun, null, 4), 'utf8');
+
+
+// all print contents:
+fs.writeFileSync('../printContents.json', JSON.stringify(allPrints, null, 4), 'utf8');
