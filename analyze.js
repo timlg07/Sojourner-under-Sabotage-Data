c757bdb64d266b01b38f5aab2d68d5f901b395f3
Tim Greller <timlg07@gmail.com>
2024-07-12 12:17:39 +0200

    change back working directory after analyzers

--- analyze.js
+++ analyze.js
@@ -1,5 +1,10 @@
-require('process').chdir('./analyzer');
+const {chdir} = require('process');
 const path = require('path');
+
+chdir('./analyzer');
+
 require('glob').sync('*.js').forEach((file) => {
     require(path.resolve(file));
 });
+
+chdir('..');
