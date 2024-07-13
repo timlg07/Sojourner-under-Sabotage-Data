81e39fd89eaf41717e584c330bdbd5fff019b7a0
Tim Greller <timlg07@gmail.com>
2024-07-13 10:27:16 +0200

    generalize json flattening for R

--- visualizer/prepareJsonForR.js
+++ visualizer/prepareJsonForR.js
@@ -1,5 +1,9 @@
 const save = require('../utils/save');
 
+const filesToFlatten = [
+    'attemptsUntilActivation',
+];
+
 const flatten = x => Object.entries(x).reduce((acc, [user, item]) => {
     Object.entries(item).forEach(([component, item]) => {
         acc.push({user, component, ...item});
@@ -7,6 +11,8 @@ const flatten = x => Object.entries(x).reduce((acc, [user, item]) => {
     return acc;
 }, []);
 
-const attemptsUntilActivation = require('../attemptsUntilActivation.json');
-const attemptsUntilActivation_r = flatten(attemptsUntilActivation);
-save('attemptsUntilActivation_r.json', attemptsUntilActivation_r);
+filesToFlatten.forEach(name => {
+    const json = require(`../${name}.json`);
+    const json_r = flatten(json);
+    save(`${name}_r.json`, json_r);
+})
--- visualizer/visualize.js
+++ visualizer/visualize.js
@@ -1,3 +1,5 @@
+require('./prepareJsonForR');
+
 const R = require('r-integration');
 const config = require('./config.json');
 const stdout = R.callMethod("./visualizer/index.R", "main", config);
