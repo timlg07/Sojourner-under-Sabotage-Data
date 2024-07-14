06efa34726e2f5861342c4746b15be22a843aa9f
Tim Greller <timlg07@gmail.com>
2024-07-14 18:34:09 +0200

    level reached: add actual amounts as numbers to the plot

--- visualizer/levelReached.R
+++ visualizer/levelReached.R
@@ -19,13 +19,13 @@ for (i in seq_len(nrow(level_reached_amount))) {
 plot <- ggplot(level_reached_amount, aes(x = value, y = count)) +
   geom_bar(stat = "identity") +
   labs(title = "highest level reached by players", x = "Level", y = "Amount of players") +
-  theme_minimal()
+  geom_text(aes(label = count), vjust = -0.5, size = 3)
 plot
 ggsave(paste0(outputDir, "level_reached.png"), plot, width = 15, height = 10, units = "cm", dpi = 300, limitsize = FALSE, device = "png")
 
 plot <- ggplot(level_played_by, aes(x = value, y = count)) +
   geom_bar(stat = "identity") +
   labs(title = "amount of players that played level x", x = "Level", y = "Amount of players") +
-  theme_minimal()
+  geom_text(aes(label = count), vjust = -0.5, size = 3)
 plot
 ggsave(paste0(outputDir, "level_played_by.png"), plot, width = 15, height = 10, units = "cm", dpi = 300, limitsize = FALSE, device = "png")
