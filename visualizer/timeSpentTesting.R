9c8189dc567cd7a3d4965b53eaed92ea1ebea0c1
Tim Greller <timlg07@gmail.com>
2024-07-15 11:46:43 +0200

    time spent on different tasks: implement fancy plots & save

--- visualizer/timeSpentTesting.R
+++ visualizer/timeSpentTesting.R
@@ -32,10 +32,40 @@ total_debugging_per_user <- time_debugging %>%
   group_by(user) %>%
   summarise(total_time = sum(deltaTime))
 
+plot_time_spent <- function (total_time_df) {
+  total_time_melted <- melt(total_time_df, id = "user")
 
-ggplot() +
-  geom_boxplot(data = total_testing_per_user, aes(y = total_time, x = "test"), color = "grey", alpha = 0.5) +
-  geom_point(data = total_testing_per_user, aes(y = total_time, x = "test")) +
-  geom_boxplot(data = total_debugging_per_user, aes(y = total_time, x = "debug"), color = "grey", alpha = 0.5) +
-  geom_point(data = total_debugging_per_user, aes(y = total_time, x = "debug")) +
-  labs(title = "Total time spent on a task per user", x = "Task", y = "Time spent in minutes")
+  plot <- ggplot(data = total_time_melted, aes(x = variable, y = value, fill = variable, group = variable)) +
+    geom_violin(alpha = .75, color = "transparent") +
+    geom_boxplot(width = .15, color = "white") +
+    labs(title = "Total time spent on each type of tasks per user",
+         x = "Type of tasks", y = "Time spent in minutes", fill = "Type of tasks", group = "Type of tasks") +
+    scale_fill_manual(values = c("#00d070", "#ff9e49", "#579ad6"), labels = c("Testing", "Debugging", "Other")) +
+    expand_limits(y = 0) +
+    theme(legend.position = "none") +
+    # display mean value at the center of the boxplot
+    stat_summary(fun = mean, geom = "point", size = 1, color = "black") +
+    stat_summary(fun = mean, geom = "text", aes(label = paste("average:\n", round(..y.., 0), "min")), vjust = -.25)
+  return(plot)
+}
+
+# build everything in one df
+total_time <- inner_join(total_testing_per_user, total_debugging_per_user, by = "user")
+total_time$test <- total_time$total_time.x
+total_time$debug <- total_time$total_time.y
+total_time$other <- 60 - total_time$test - total_time$debug
+total_time <- total_time[, c("user", "test", "debug", "other")]
+
+
+plot <- plot_time_spent(total_time)
+plot
+ggsave(filename = paste0(outputDir, "timeSpentOnTasks.png"), plot, width = 10, height = 8)
+
+# restrict to only people who reached level 4
+level_reached <- fromJSON(txt = "./visualizer/r_json/levelReached_r.json", flatten = TRUE)
+users_that_reached_level4 <- level_reached %>%
+  filter(value > 3) %>%
+  select(user)
+total_time <- inner_join(total_time, users_that_reached_level4, by = "user")
+
+# plot_time_spent(total_time)
