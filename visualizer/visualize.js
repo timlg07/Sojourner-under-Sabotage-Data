ed8a94113afefa3c301b22ed5f3f5d21d81e214b
Tim Greller <timlg07@gmail.com>
2024-07-16 15:58:46 +0200

    compressed output of the R stdout

--- visualizer/visualize.js
+++ visualizer/visualize.js
@@ -3,4 +3,4 @@ require('./prepareJsonForR');
 const R = require('r-integration');
 const config = require('./config.json');
 const stdout = R.callMethod("./visualizer/index.R", "main", config);
-console.log(stdout);
+console.log(stdout.join(" "));
