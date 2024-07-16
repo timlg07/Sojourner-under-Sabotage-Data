9d0e8d83df9bb334062f340d1996ec8c6203ab0a
Tim Greller <timlg07@gmail.com>
2024-07-16 15:58:18 +0200

    additional attempts until fixed plot considering all data points, not just the averages

--- visualizer/attemptsUntilFixed.R
+++ visualizer/attemptsUntilFixed.R
@@ -9,7 +9,8 @@ if (!exists("outputDir")) outputDir <- "./visualizer/out/"
 debugging <- fromJSON(txt = "./visualizer/r_json/attemptsUntilFixed_summary_r.json", flatten = TRUE) %>%
   filter(deltaTime != "not fixed") %>%
   mutate(deltaTime = as.numeric(deltaTime)) %>%
-  filter(deltaTime < 55) # continued playing at home
+  filter(deltaTime < 55) %>% # continued playing at home
+  select(user, componentName, deltaTime, modifications, executions, hiddenTestsAdded)
 
 debugging_per_component <- debugging %>%
   group_by(componentName) %>%
@@ -21,12 +22,26 @@ debugging_per_component_melted <- melt(debugging_per_component, id = "componentN
 ggplot(data = debugging_per_component_melted, aes(x = componentName, group = variable, y = value)) +
   geom_line(aes(color = variable)) +
   geom_point(aes(color = variable)) +
-  labs(title = "Average time, amount of modifications, executions and\nhidden tests added until the component was fixed",
+  labs(title = "Average time, amount of modifications, executions and hidden tests added until the component was fixed",
        x = "Component", y = "Average time spent debugging in minutes", color = "Type of metric") +
   scale_color_manual(values = c("red", "orange", "green", "blue"), labels = c("Time", "Modifications", "Executions", "Hidden tests added")) +
   scale_y_continuous(sec.axis = sec_axis(~., name = "Amount", breaks = seq(0, 10, 1)), breaks = seq(0, 10, 1))
-ggsave(filename = paste0(outputDir, "debugging_performance_per_component.png"), width = 10, height = 5)
+ggsave(filename = paste0(outputDir, "debugging_performance_per_component.png"), width = 12, height = 5)
 
 averages <- debugging %>%
   summarise(avg_time = mean(deltaTime), avg_mod = mean(modifications),
             avg_exc = mean(executions), avg_hid = mean(hiddenTestsAdded))
+
+
+debugging_melted <- melt(debugging, id = c("componentName", "user")) %>%
+  filter(variable != "executions")
+ggplot(data = debugging_melted, aes(x = componentName, y = value, color = variable, group = interaction(componentName, variable))) +
+  #geom_violin() +
+  geom_boxplot(width = .5) +
+  geom_smooth(method = "loess", se = FALSE, formula = y ~ x, aes(group = variable), linetype = "dashed", size = .5) +
+  #geom_point()+
+  labs(title = "Time spent debugging per component", x = "Component", y = "Time spent in minutes",
+       fill = "Type of metric", group = "Type of metric") +
+  scale_color_manual(values = c("red", "blue", "orange"), labels = c("Time", "Modifications", "Hidden tests added")) +
+  scale_y_continuous(sec.axis = sec_axis(~., name = "Amount", breaks = seq(0, 100, 2)), breaks = seq(0, 100, 2))
+ggsave(filename = paste0(outputDir, "debugging_performance_per_component_boxplots.png"), width = 12, height = 8)
