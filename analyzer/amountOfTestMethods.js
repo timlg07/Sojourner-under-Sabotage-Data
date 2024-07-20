fdd01e8f71a79acbff2aa184913ed13148dd74cd
Tim Greller <timlg07@gmail.com>
2024-07-20 13:52:41 +0200

    do not only count the amount of @Test annotations but also the amount of methods to find out how many annotations were missed

--- analyzer/amountOfTestMethods.js
+++ analyzer/amountOfTestMethods.js
@@ -16,12 +16,35 @@ function countTestMethods(src) {
     return count;
 }
 
+function countAllMethods(src) {
+    let count = 0;
+    for (const line of src.split('\n')) {
+        if (/^\s*public\s+void\s+\w+\(\s*\)\s*\{/.test(line)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const delta = {count: 0, componentNames: [], code: []};
 const amountOfTestMethods = Object.entries(data).reduce((acc, item) => {
     acc[item[0]] = Object.entries(item[1]).reduce((acc, item) => {
-        acc[item[0]] = countTestMethods(stripComments(item[1]));
+        const src = stripComments(item[1]);
+        acc[item[0]] = {
+            methods: countAllMethods(src),
+            testAnnotations: countTestMethods(src)
+        };
+        const currentDelta = acc[item[0]].methods - acc[item[0]].testAnnotations;
+        if (currentDelta > 0) {
+            delta.count += currentDelta;
+            delta.componentNames.push(item[0]);
+            delta.code.push([src]);
+        }
         return acc;
     }, {});
     return acc;
 }, {});
 
 save('../amountOfTestMethods.json', amountOfTestMethods);
+
+// console.log('Delta:', delta);
