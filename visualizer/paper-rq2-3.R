1844648129bd6c7e18430f9136a69cc8361176bf
Tim Greller <mail@tim-greller.de>
2025-01-21 00:20:36 +0100

    rq2_3_number_of_test_methods_per_component_per_user_per_component:  combined plot for ST, SE

--- visualizer/paper-rq2-3.R
+++ visualizer/paper-rq2-3.R
@@ -18,33 +18,50 @@ if (!exists("presentationDir")) presentationDir <- "./visualizer/out/"
 # (3) Number of tests written per component -----------------------------------#
 
 methods <- fromJSON(txt = "./visualizer/r_json/amountOfTestMethods_r.json", flatten = TRUE) %>%
-  select(user, componentName, value = testAnnotations)
+  select(user, componentName, value = testAnnotations) %>%
+  levelNumbers() %>%
+  splitDataByTestGroup()
 
 
-methods_per_component_avg <- methods %>%
-  group_by(componentName) %>%
-  summarise(avg = mean(value))
-
-methods_per_component_total <- methods %>%
-  group_by(componentName) %>%
-  summarise(total = sum(value))
-
-plot <- ggplot(data = levelNumbers(methods_per_component_avg), aes(x = componentName, y = avg)) +
-  theme_minimal() +
-  geom_bar(stat = "identity", fill = "#B8A0F8") +
-  labs(#title = "Average amount of test methods per component",
-    x = element_blank(), y = "Average number of tests") +
-  geom_text(aes(label = paste("Total:", methods_per_component_total %>%
-    filter(componentName == as.character(componentName)) %>%
-    pull(total))), vjust = 1.5, size = 3, color = "white")
-plot + geom_text(aes(label = paste("Average:", round(avg, 1))), vjust = -0.5, size = 3)
+# methods_per_component_avg <- methods %>%
+#   group_by(componentName) %>%
+#   summarise(avg = mean(value))
+#
+# methods_per_component_total <- methods %>%
+#   group_by(componentName) %>%
+#   summarise(total = sum(value))
+#
+# plot <- ggplot(data = levelNumbers(methods_per_component_avg), aes(x = componentName, y = avg)) +
+#   theme_minimal() +
+#   geom_bar(stat = "identity", fill = "#B8A0F8") +
+#   labs(#title = "Average amount of test methods per component",
+#     x = element_blank(), y = "Average number of tests") +
+#   geom_text(aes(label = paste("Total:", methods_per_component_total %>%
+#     filter(componentName == as.character(componentName)) %>%
+#     pull(total))), vjust = 1.5, size = 3, color = "white")
+# plot + geom_text(aes(label = paste("Average:", round(avg, 1))), vjust = -0.5, size = 3)
 
 #violins
-plot <- ggplot(data = levelNumbers(methods), aes(x = componentName, y = value)) +
+ggplot(data = methods, aes(
+  x = group,
+  y = value,
+  fill = group,
+  color = group
+)) +
   theme_minimal() +
-  geom_violin(color = "transparent", fill = colors[1], alpha = .5) +
-  geom_boxplot(width = .1, color = colors[5], fill = "white") +
+  geom_violin(color = "transparent", alpha = .5) +
+  geom_boxplot(width = .1) +
   labs(x = element_blank(), y = "Number of tests") +
-  scale_y_continuous(breaks = seq(0, 20, 1))
-plot
-ggsave(paste0(outputDir, "paper/rq2_3_number_of_test_methods_per_component_per_user_per_component.png"), width = 5, height = 4)
+  scale_y_continuous(breaks = seq(0, 20, 1)) +
+  scale_fill_manual(values = colors[1:2]) +
+  scale_color_manual(values = colors[c(5,4)]) +
+
+  scale_x_discrete(labels = c("ST", "SE")) +
+  facet_grid(~ componentName, switch = "x") +
+  theme(panel.spacing.x = grid::unit(2, "mm"),
+        strip.placement = "outside",
+        strip.background = element_blank(),
+        legend.position = "none"
+  )
+
+ggsave(paste0(outputDir, "paper/rq2_3_combined_number_of_test_methods_per_component_per_user_per_component.png"), width = 5, height = 4)
