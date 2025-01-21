e0121e15ea7fc4fc97c890ac97e8f94d0b0814e9
Tim Greller <mail@tim-greller.de>
2025-01-21 14:17:34 +0100

    redo 2_5 coverage_vs_target_killed_regression

--- visualizer/paper-rq2-5.R
+++ visualizer/paper-rq2-5.R
@@ -5,6 +5,7 @@ library(ggdark)
 library(dplyr)
 library(purrr)
 library(reshape2)
+library(ggpmisc)
 
 source("./visualizer/utils.R")
 
@@ -17,10 +18,14 @@ if (!exists("presentationDir")) presentationDir <- "./visualizer/out/"
 
 # (5) Correlation between coverage and target mutant killed -------------------#
 coverage <- fromJSON(txt = "./visualizer/r_json/coverageAtActivation_r.json", flatten = TRUE) %>%
-  filter(componentName != "ReactorLog")
+  group_by(user) %>%
+  filter(n() > 1) %>%
+  ungroup()
 
 destroyed_or_alarm <- fromJSON(txt = "./visualizer/r_json/destroyedOrAlarm_r.json", flatten = TRUE) %>%
-  filter(componentName != "ReactorLog")
+  group_by(user) %>%
+  filter(n() > 1) %>%
+  ungroup()
 
 
 cov_and_result <- destroyed_or_alarm
@@ -34,18 +39,35 @@ coverage_per_player <- coverage %>%
   summarise(fraction = mean(fraction))
 cov_and_result <- inner_join(coverage_per_player, cov_and_result, by = "user")
 
+cov_and_result <- cov_and_result %>% splitDataByTestGroup()
+cov_and_result_SE <- cov_and_result %>% filter(group == "1_SE")
+cov_and_result_ST <- cov_and_result %>% filter(group == "2_ST")
 
 set.seed(42)
-plot <- ggplot(data = cov_and_result, aes()) +
-  theme_minimal() +
-  geom_point(aes(x = fraction, y = destroyed, color = "Destroyed"), pch = 20, alpha = .5, size = 6, position = position_jitter(width = 0, height = 0.05)) +
-  geom_smooth(aes(x = fraction, y = destroyed, color = "Destroyed"), method = "lm", se = FALSE, formula = y ~ x) +
-  geom_point(aes(x = fraction, y = alarm, color = "Alarm"), pch = 20, alpha = .5, size = 6, position = position_jitter(width = 0, height = 0.05)) +
-  geom_smooth(aes(x = fraction, y = alarm, color = "Alarm"), method = "lm", se = FALSE, formula = y ~ x) +
-  labs(x = "Average test coverage", y = "Number of events", color = "Event result") +
-  scale_color_manual(values = colors[c(3, 5)], labels = c("Alarm (Mutant detected)", "Destroyed (Mutant not detected)")) +
-  scale_x_continuous(labels = scales::percent_format(scale = 100)) +
-  scale_y_continuous(breaks = seq(0, 13, 1), minor_breaks = numeric(0)) +
-  theme(legend.position = "bottom")
-plot
-ggsave(filename = paste0(outputDir, "paper/rq2_5_coverage_vs_target_killed_regression__per_player.png"), width = 6, height = 4)
+plot_cov_vs_result <- function(data)
+  ggplot(data = data, aes()) +
+    theme_minimal() +
+    geom_point(aes(x = fraction, y = destroyed, color = "Destroyed"), pch = 20, alpha = .5, size = 6, position = position_jitter(width = 0, height = 0.05)) +
+    geom_point(aes(x = fraction, y = alarm, color = "Alarm"), pch = 20, alpha = .5, size = 6, position = position_jitter(width = 0, height = 0.05)) +
+
+    stat_poly_line(aes(x = fraction, y = destroyed, color = "Destroyed"), method = "lm", se = FALSE, formula = y ~ x) +
+    stat_poly_eq(aes(x = fraction, y = destroyed, color = "Destroyed",
+                     label = paste(..rr.label.., ..p.value.label.., sep = "*`,`~")),
+                 label.y = 0.85, label.x = 0.5) +
+
+    stat_poly_line(aes(x = fraction, y = alarm, color = "Alarm"), method = "lm", se = FALSE, formula = y ~ x) +
+    stat_poly_eq(aes(x = fraction, y = alarm, color = "Alarm",
+                     label = paste(..rr.label.., ..p.value.label.., sep = "*`,`~")),
+                 label.y = 0.075, label.x = 0.5) +
+
+    labs(x = "Average test coverage", y = "Number of events", color = "Event result") +
+    scale_color_manual(values = colors[c(3, 5)], labels = c("Alarm (Mutant detected)", "Destroyed (Mutant not detected)")) +
+    scale_x_continuous(labels = scales::percent_format(scale = 100)) +
+    scale_y_continuous(breaks = seq(0, 13, 1), minor_breaks = numeric(0)) +
+    theme(legend.position = "bottom")
+
+plot_cov_vs_result(cov_and_result_SE)
+ggsave(filename = paste0(outputDir, "paper/rq2_5_coverage_vs_target_killed_regression__SE.png"), width = 6, height = 4)
+
+plot_cov_vs_result(cov_and_result_ST)
+ggsave(filename = paste0(outputDir, "paper/rq2_5_coverage_vs_target_killed_regression__ST.png"), width = 6, height = 4)
